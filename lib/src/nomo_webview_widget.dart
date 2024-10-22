2ea1b768302c544f7c085b79bd6e6250f0c24a65
nomo-fe <dev2@nomo.app>
2024-10-22 18:36:54 +0200

    feat: re-introduce a version that is based on webview_flutter instead of flutter_inappwebview

--- lib/src/nomo_webview_widget.dart
+++ lib/src/nomo_webview_widget.dart
@@ -1,33 +1,17 @@
 import 'package:flutter/widgets.dart';
 import 'package:nomo_webview/nomo_webview.dart';
 
-import 'package:flutter/material.dart';
-
 /// NomoWebViewWidget attempts to refresh a valid BuildContext for usage by JavaScript invocations.
-
-class NomoWebViewWidget extends StatelessWidget {
+class NomoWebViewWidget extends WebViewWidget {
   final NomoController nomoController;
 
-  const NomoWebViewWidget({Key? key, required this.nomoController});
+  NomoWebViewWidget({super.key, required this.nomoController})
+      : super(controller: nomoController.c);
 
   @override
   Widget build(BuildContext context) {
     // we assume that WebViewControllers are "long-lived" whereas WebViewWidgets are "short-lived"
     nomoController.updateBuildContext(context);
-    return InAppWebView(
-      key: key,
-      keepAlive: nomoController.keepAlive,
-      initialUrlRequest: nomoController.initialUrlRequest,
-      initialSettings: nomoController.initialSettings,
-      initialUserScripts: nomoController.initialUserScripts,
-      onWebViewCreated: nomoController.onWebViewCreated,
-      onReceivedHttpError: nomoController.onReceivedHttpError,
-      onConsoleMessage: nomoController.onConsoleMessage,
-      shouldOverrideUrlLoading: nomoController.shouldOverrideUrlLoading,
-      onDownloadStartRequest: nomoController.onDownloadStartRequest,
-      onLoadResourceWithCustomScheme:
-          nomoController.onLoadResourceWithCustomScheme,
-      onPermissionRequest: nomoController.onPermissionRequest,
-    );
+    return super.build(context);
   }
 }
--- lib/src/webviewcontroller_extension.dart
+++ lib/src/webviewcontroller_extension.dart
@@ -1,102 +1,21 @@
 import 'dart:async';
-import 'dart:collection';
 
 import 'package:flutter/widgets.dart';
 import 'package:nomo_webview/nomo_webview.dart';
-
 import 'package:flutter/material.dart';
 
 final Map<NomoController, BuildContext> _contextMap = {};
 
 class NomoController {
-  InAppWebViewController? inAppController;
+  WebViewController c;
   int? localServerPort;
 
   NomoController({
-    this.initialSettings,
-    this.initialUrlRequest,
-    this.initialUserScripts,
-    this.onWebviewInit,
-    this.onDownloadStartRequest,
-    this.onReceivedHttpError,
-    this.onLoadResourceWithCustomScheme,
-    this.onLoadStopInternal,
-    this.shouldOverrideUrlLoadingInternal,
+    required this.c,
   });
 
-  final keepAlive = InAppWebViewKeepAlive();
-  //InAppWebViewKeepAlive? keepAlive;
-  void onWebViewCreated(controller) {
-    inAppController = controller;
-    if (onWebviewInit != null) {
-      onWebviewInit!(this);
-    }
-  }
-
-  URLRequest? initialUrlRequest;
-  UnmodifiableListView<UserScript>? initialUserScripts;
-
-  InAppWebViewSettings? initialSettings;
-  void Function(NomoController nomoControl)? onWebviewInit;
-  void onConsoleMessage(
-      InAppWebViewController controller, ConsoleMessage consoleMessage) {
-    print(consoleMessage);
-  }
-
-  void Function(InAppWebViewController controller,
-      DownloadStartRequest downloadStartRequest)? onDownloadStartRequest;
-
-  void Function(InAppWebViewController controller, WebResourceRequest request,
-      WebResourceResponse errorResponse)? onReceivedHttpError;
-
-  Future<CustomSchemeResponse?> Function(
-          InAppWebViewController controller, WebResourceRequest request)?
-      onLoadResourceWithCustomScheme;
-
-  void Function(NomoController nomoControl, InAppWebViewController controller,
-      WebUri? url)? onLoadStopInternal;
-
-  void Function(NomoController nomoControl, InAppWebViewController controller,
-      WebUri? url)? onLoadStartInternal;
-
-  Future<NavigationActionPolicy?> Function(
-      NomoController nomoControl,
-      InAppWebViewController controller,
-      NavigationAction navigationAction)? shouldOverrideUrlLoadingInternal;
-
-  Future<NavigationActionPolicy?> shouldOverrideUrlLoading(
-      InAppWebViewController controller,
-      NavigationAction navigationAction) async {
-    return shouldOverrideUrlLoadingInternal != null
-        ? await shouldOverrideUrlLoadingInternal!(
-            this, controller, navigationAction)
-        : NavigationActionPolicy.ALLOW;
-  }
-
-  Future<PermissionResponse?> onPermissionRequest(
-      InAppWebViewController controller,
-      PermissionRequest permissionRequest) async {
-    return PermissionResponse(
-        resources: permissionRequest.resources,
-        action: PermissionResponseAction.GRANT);
-  }
-
-  Future<dynamic> evaluateJavascript(
-      {required String source, ContentWorld? contentWorld}) {
-    return inAppController!
-        .evaluateJavascript(source: source, contentWorld: contentWorld);
-  }
-
-  void addJavaScriptHandler(
-      {required String handlerName,
-      required JavaScriptHandlerCallback callback}) {
-    return inAppController!
-        .addJavaScriptHandler(handlerName: handlerName, callback: callback);
-  }
-
-  Future<void> setWebContentsDebuggingEnabled(bool debuggingEnabled) {
-    return InAppWebViewController.setWebContentsDebuggingEnabled(
-        debuggingEnabled);
+  Future<dynamic> evaluateJavascript({required String source}) {
+    return c.runJavaScript(source);
   }
 
   void updateBuildContext(BuildContext context) {
@@ -128,61 +47,24 @@ class NomoController {
       await evaluateJavascript(source: jsCode);
     }
 
-    addJavaScriptHandler(
-        handlerName: 'NOMOJSChannel',
-        callback: (message) async {
-          // https://stackoverflow.com/questions/53689662/flutter-webview-two-way-communication-with-javascript
-          final raw = message.toString();
-          final messageFromJs = raw.substring(1, raw.length - 1);
-          if (rawMessageHandler != null) {
-            rawMessageHandler(messageFromJs);
-          }
-          handleMessageFromJavaScript(
-            messageFromJs: messageFromJs,
-            argsFromDart: argsFromDart,
-            jsHandler: jsHandler,
-            jsInjector: jsInjector,
-            // we prefer the context from the last build because we assume it is more likely to be valid
-            context: getBuildContext(),
-          );
-          return {'test': 'test_val'};
-        });
+    c.addJavaScriptChannel('NOMOJSChannel', onMessageReceived: (message) async {
+      // https://stackoverflow.com/questions/53689662/flutter-webview-two-way-communication-with-javascript
+      final messageFromJs = message.message;
+      if (rawMessageHandler != null) {
+        rawMessageHandler(messageFromJs);
+      }
+      handleMessageFromJavaScript(
+        messageFromJs: messageFromJs,
+        argsFromDart: argsFromDart,
+        jsHandler: jsHandler,
+        jsInjector: jsInjector,
+        // we prefer the context from the last build because we assume it is more likely to be valid
+        context: getBuildContext(),
+      );
+    });
   }
 
   Future<dynamic> runJavaScript(String code) async {
     return await evaluateJavascript(source: code);
-  }
-
-  void injectIpfsScheme() {
-    runJavaScript("""
-      function replaceWithIPFSGateway() {
-          // Get all elements with src attribute
-          var elements = document.querySelectorAll('[src]');
-          
-          // Loop through each element
-          elements.forEach(function(element) {
-              // Get the original source URL
-              var originalSrc = element.getAttribute('src');
-              
-              // Check if the source URL starts with "ipfs://"
-              if (originalSrc.startsWith('ipfs://')) {
-                  // Extract the IPFS CID from the source URL
-                  var ipfsCID = originalSrc.replace('ipfs://', '');
-                  
-                  // Construct the HTTP gateway URL
-                  var gatewayURL = 'https://ipfs.io/ipfs/';
-                  
-                  // Construct the final URL by combining the gateway URL and the IPFS CID
-                  var finalURL = gatewayURL + ipfsCID;
-                  
-                  // Replace the original source URL with the final URL
-                  element.setAttribute('src', finalURL);
-              }
-          });
-      }
-
-      // Call the function to replace source URLs with IPFS gateway URLs when the page is loaded
-      window.onload = replaceWithIPFSGateway;
-      """);
   }
 }
